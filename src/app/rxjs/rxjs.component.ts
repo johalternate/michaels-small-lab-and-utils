4ca8eff7e3bf6b0739d9ed957387f1c59cf17149
Michael Small <chickenhat22@gmail.com>
2025-04-05 13:17:47 -0500

    feat: add `switchMap` example

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,16 +1,32 @@
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject, Observable, of, switchMap } from 'rxjs';
 
+type UserInfo = {
+    id: number, name: string, hobbies: string
+}
 @Component({
   selector: 'app-rxjs',
-  imports: [],
+  imports: [AsyncPipe, JsonPipe],
   template: `
-    <p>
-      rxjs works!
-    </p>
+    <h1><code>switchMap</code></h1>
+    <button (click)="currentUser$.next('Dave')">Set Dave</button>
+    <button (click)="currentUser$.next('Jeff')">Set Jeff</button>
+
+    <pre>{{userInfo$ | async | json}}</pre>
   `,
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RxjsComponent {
+    currentUser$ = new BehaviorSubject<'Dave' | 'Jeff'>('Dave');
 
+    getUserInfo(user: 'Dave' | 'Jeff'): Observable<UserInfo> {
+        const userInfo = user === 'Dave' ? {id: 1, name: 'Dave', hobbies: 'fishing'}: {id: 2, name: 'Jeff', hobbies: 'bowling'}
+        return of(userInfo)
+    }
+
+    userInfo$: Observable<UserInfo> = this.currentUser$.pipe(
+        switchMap(user => this.getUserInfo(user))
+    )
 }
